b9022ee3f96baa82c42513d0a0cbf09d60371ba6
Eren Çataltepe <erencataltepe@gmail.com>
2021-07-09 11:43:48 +0300

    Create header.js

---
+++ src/header.js
@@ -0,0 +1,22 @@
+//create html elements
+const header = document.createElement("header");
+const restaurantName = document.createElement("div");
+const slogan = document.createElement("div");
+const h1Header = document.createElement("h1");
+const h4Header = document.createElement("h4");
+
+//Add id to the elements
+restaurantName.setAttribute("id", "restaurant-name");
+slogan.setAttribute("id", "slogan");
+
+//Add content to the elements
+h1Header.textContent = "Aegean Bistro";
+h4Header.textContent =
+  "Delicious meals and refreshing drinks from ancient lands of Aegean";
+//Add header sub childs to the header element
+restaurantName.appendChild(h1Header);
+slogan.appendChild(h4Header);
+header.appendChild(restaurantName);
+header.appendChild(slogan);
+
+export { header };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import { header } from "./header";
 
+const content = document.getElementById("content");
+content.appendChild(header);
+console.log(header);
