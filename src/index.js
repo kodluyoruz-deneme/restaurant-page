02bf6f94a3051465db33d12fb341cfcac68d316e
Eren Çataltepe <erencataltepe@gmail.com>
2021-07-09 16:16:26 +0300

    Create menu section.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { loadHeader } from "./header";
 import { loadMainPart } from "./main";
 import { loadHome } from "./home";
+import { loadMenu } from "./menu";
 
 function initialLoad() {
   const header = loadHeader();
-  const main = loadMainPart(loadHome);
+  const main = loadMainPart(loadMenu);
   const content = document.getElementById("content");
   content.appendChild(header);
   content.appendChild(main);
